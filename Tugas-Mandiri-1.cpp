f4ffc9807126bcf9b966cfe684db1a63b91bd6e9
Ristanto <ristantohermawan@gmail.com>
2024-10-04 11:01:44 +0700

    Add files via upload

---
+++ Tugas-Mandiri-1.cpp
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() 
+{
+
+    char nim[] = "2441040";
+    char nama[] = "Ristanto Hermawan Saputra";
+    char jurusan[] = "Teknik Informatika";
+    
+    printf("NIM: %s\n", nim);
+    printf("Nama: %s\n", nama);
+    printf("Jurusan: %s\n", jurusan);
+    
+    return 0;
+}    
---
+++ tugas-mandiri-2.cpp
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main()
+{
+    int panjang,lebar,luas;
+    panjang = 10;
+    lebar = 7;
+    luas = panjang * lebar;
+    printf("jadi luas persegi panjang adalah %i", luas);
+    return 0;
+}    
